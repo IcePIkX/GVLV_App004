24e13da9b800a91b94f2fc1a0573d95a6e484d5a
IcePIkX <15325061080098@cetis108.edu.mx>
2017-06-03 15:24:44 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/cetis108/gvlv_app004/MapsActivity.java
@@ -0,0 +1,56 @@
+package mx.edu.cetis108.gvlv_app004;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void agregarMarcador(double lat, double lng)
+    {
+        LatLng coordenadas= new LatLng(lat,lng);
+        Object cameraUpdate;
+        CameraUpdate ubicacion= CameraUpdateFactory.newLatLngZoom(coordenadas,16);
+        if (marcador!=null)
+        {
+            marcador.remove();
+        }
+        marcador=mMap.addMarker(newMarkerOptions()
+        .position(coordenadas)
+        .title("MI posicion actual")
+        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.animateCamera(ubicacion);
+    }
+}
